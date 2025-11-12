dc445bc950bb97cf7a77adccd4d074cd2f2c3bcf
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-11-12 10:16:48 -0800

    initial commit 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
-from .models import Book, Library
+from .models import Library
 
 
 def list_books(request):
