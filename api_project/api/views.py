85c8bf9aa08108778836fd11e7e2d926f1648466
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-11-23 03:58:48 -0800

    Initial commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,3 +9,7 @@ class BookList(generics.ListAPIView):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    """API viewset to retrieve, create, update, and delete books."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
