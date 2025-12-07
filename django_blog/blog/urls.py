d48c54252a4fded38642222f75a9a59fbc0cefc8
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-12-07 02:23:13 -0800

    initial commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:pk>/comment/new/', views.CommentCreateView.as_view(), name='add-comment'),
-    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit-comment'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='add-comment'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit-comment'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),
 ]
