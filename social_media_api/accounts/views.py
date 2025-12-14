f56f4a075b77d1e123955985e192a50dbe536d1a
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-12-13 22:40:15 -0800

    initial commit

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status
+from rest_framework.authtoken.models import Tokens
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return Response(
+                {'detail': 'Invalid credentials'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
